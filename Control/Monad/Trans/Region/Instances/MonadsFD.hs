f4348a704e842b4e8046b656addd36b9e1ed70ea
Bas van Dijk <v.dijk.bas@gmail.com>
2009-12-20 23:43:59 +0100

    Initial import
    Ignore-this: 66f9faec10a7c84140679b14dd843399
    
    darcs-hash:20091220224359-ae560-8c3dc4b50f9c8d1cbf0918f772c0b289a2a1ab1a.gz

---
+++ Control/Monad/Trans/Region/Instances/MonadsFD.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+-------------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Monad.Trans.Region.Instances.MonadsFD
+-- Copyright   :  (c) 2009 Bas van Dijk
+-- License     :  BSD3 (see the file LICENSE)
+-- Maintainer  :  Bas van Dijk <v.dijk.bas@gmail.com>
+--
+-- This module provides instances for the monads-fd classes for 'RegionT's.
+--
+--------------------------------------------------------------------------------
+
+module Control.Monad.Trans.Region.Instances.MonadsFD where
+
+-- from monads-fd:
+-- TODO: import Control.Monad.Cont.Class   ( MonadCont, callCC )
+import Control.Monad.Error.Class  ( MonadError, throwError, catchError )
+import Control.Monad.RWS.Class    ( MonadRWS )
+import Control.Monad.Reader.Class ( MonadReader, ask, local )
+import Control.Monad.State.Class  ( MonadState, get, put )
+import Control.Monad.Writer.Class ( MonadWriter, tell, listen, pass )
+
+-- from transformers:
+import Control.Monad.Trans ( lift )
+
+-- from unicode-symbols:
+import Prelude.Unicode ( (∘) )
+
+-- from regions:
+import Control.Monad.Trans.Region.Internal ( RegionT
+                                           , liftCatch
+                                           , mapRegionT
+                                           -- TODO: , liftCallCC
+                                           )
+
+-- TODO:
+-- instance Monad pr ⇒ MonadCont (RegionT resource s pr) where
+--     callCC = liftCallCC callCC
+
+instance MonadError e pr ⇒ MonadError e (RegionT resource s pr) where
+    throwError = lift ∘ throwError
+    catchError = liftCatch catchError
+
+instance MonadRWS r w st pr ⇒ MonadRWS r w st (RegionT resource s pr)
+
+instance MonadReader r pr ⇒ MonadReader r (RegionT resource s pr) where
+    ask   = lift ask
+    local = mapRegionT ∘ local
+
+instance MonadWriter w pr ⇒ MonadWriter w (RegionT resource s pr) where
+    tell   = lift ∘ tell
+    listen = mapRegionT listen
+    pass   = mapRegionT pass
+
+instance MonadState st pr ⇒ MonadState st (RegionT resource s pr) where
+    get = lift get
+    put = lift ∘ put
